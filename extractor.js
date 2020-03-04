6eb06dc21e118665208f61f317a7ce580b3c957e
L. Alexandre <aleblanc@interact.lu>
2020-03-04 10:18:32 +0100

    Update extractor.js

--- extractor.js
+++ extractor.js
@@ -1,7 +1,7 @@
 
 output='<html><head><title>SEO 404 Extraction Tool</title><style type=\'text/css\'>body,table{font-family:Tahoma,Verdana,Segoe,sans-serif;font-size:11px;color:#000}h1,h2,th{color:#405850}th{text-align:left}h2{font-size:11px;margin-bottom:3px}.status_404{colore:red;}</style></head><body>';
 
-output+="<h1>SEO SERP Extraction Tool</h1>";
+output+="<h1>SEO 404 Extraction Tool</h1>";
 
 (function(e, s) {
     e.src = s;
